b1bf3ec49b766939265284cb3d6c0ede70a23de1
Eryhnar <pedrofernbel@gmail.com>
2024-02-26 19:31:15 +0100

    feat: database connected

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import "dotenv/config";
 import { app } from "./app";
+import { AppDataSource } from "./db";
 
 const PORT = process.env.PORT || 4001;
 
 const startServer = () => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port: ${PORT}`);
+    AppDataSource.initialize()
+    .then(() => {
+        console.log("Database connected");
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to database: ", error);
     });
 }
 
