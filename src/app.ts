1d266580b6e1c300bca082f3085cf08bef7f3b69
Eryhnar <pedrofernbel@gmail.com>
2024-02-27 18:20:35 +0100

    feat: Role model created.

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,22 @@ import express from 'express';
 export const app = express();
 
 app.use(express.json());
+
+/*
+app.post('/users', validateUser, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password, role_id } = req.body;
+
+    const newUser = await User.create({
+        name,
+        email,
+        password,
+        role_id
+    });
+    res.json(newUser);
+});
+
+*/ 
---
+++ src/models/Role.ts
@@ -0,0 +1,12 @@
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class Role extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id!: number;
+    @Column()
+    name!: string;
+
+    @OneToMany(() => User, user => user.role)
+    users!: User[];
+}
