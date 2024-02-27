eba8c31ebb4de73c04bec5ac86a1c4ccc494c05d
Eryhnar <pedrofernbel@gmail.com>
2024-02-27 19:34:15 +0100

    feat: Service model created.

---
+++ src/models/Service.ts
@@ -0,0 +1,22 @@
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class Service extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ name: "name", unique: true})
+    name!: string;
+    
+    @Column({ name: "description"})
+    description!: string;
+    
+    @Column({ name: "price"})
+    price!: number;
+
+    @OneToMany(() => Catalogue, catalogue => catalogue.id)
+    catalogues!: Catalogue[];
+
+    @OneToMany(() => Appointment, appointment => appointment.id)
+    appointments!: Appointment[];
+}
