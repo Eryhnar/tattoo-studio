e2ea0d12442dadcc3af21b0a22402cf5ace04aeb
Eryhnar <pedrofernbel@gmail.com>
2024-02-28 19:58:51 +0100

    feat: updateUserById route added.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import express from "express";
 import { getRoles, updateRole, createRole } from "./controllers/role-controller";
 import { register } from "./controllers/auth-controller";
-import { getUsers } from './controllers/user-controller';
+import { getUsers, updateUserById } from "./controllers/user-controller";
 
 export const app = express();
 
@@ -17,3 +17,4 @@ app.post("/api/register", register);
 
 //user routes
 app.get("/api/users", getUsers);
+app.put("/api/users/:id", updateUserById);
--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { User } from "../models/User";
+import { validateEmail, validateUserName } from "../helpers/validation-utilities";
 
 export const getUsers = async (req: Request, res: Response) => {
     try {
@@ -31,5 +32,44 @@ export const getUsers = async (req: Request, res: Response) => {
             message: "Error fetching users",
             error: error
         });
+    }
+};
+
+export const updateUserById = async (req: Request, res: Response) => { //update multiple fields. Should I update one field at a time? // ask for login before updating??
+    try {
+        const targetUserId = parseInt(req.params.id);
+        let { name, email } = req.body;
+
+        const targetUser = await User.findOneBy({ id: targetUserId });
+        if (!targetUser) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+
+        let updateFields: { [key: string]: string } = {};
+
+        if (name) {
+            name = name.trim();
+            updateFields["name"] = name;
+        }
+
+        if (email) {
+            email = email.trim();
+            updateFields["email"] = email;
+        }
+
+        if (updateFields["name"] && !validateUserName(updateFields["name"])) {
+            return res.status(400).json({ success: false, message: "Invalid user name. Only unicode characters are allowed and it must not exceed 50 characters" });
+        }
+
+        if (updateFields["email"] && !validateEmail(updateFields["email"])) {
+            return res.status(400).json({ success: false, message: "Invalid email" });
+        }
+
+        await User.update({ id: targetUserId }, updateFields);
+
+        return res.status(200).json({ success: true, message: "User updated successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Error updating user", error: error });
     }
 };
