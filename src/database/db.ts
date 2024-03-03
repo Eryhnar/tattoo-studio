c4526810a62855f0adb219aad0280726df0d0335
Eryhnar <pedrofernbel@gmail.com>
2024-03-03 23:49:48 +0100

    feat: started migrating validations to middlewares.

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,13 +4,13 @@ import "dotenv/config";
 import { Roles1708974926321 } from "./migrations/1708974926321-roles";
 import { Users1708975325676 } from "./migrations/1708975325676-users";
 import { Services1708976078071 } from "./migrations/1708976078071-services";
-import { Appointments1708976549606 } from "./migrations/1708976549606-appointments";
 import { Catalogue1708977049545 } from "./migrations/1708977049545-catalogue";
 import { Role } from "../models/Role"; 
 import { User } from "../models/User"; 
 import { Service } from "../models/Service"; 
 import { Catalogue } from "../models/Catalogue"; 
 import { Appointment } from "../models/Appointment";
+import { Appointments1708978549606 } from "./migrations/1708978549606-appointments";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -20,7 +20,7 @@ export const AppDataSource = new DataSource({
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_NAME || "tattoo_studio",
     entities: [Role, User, Service, Catalogue, Appointment],
-    migrations: [Roles1708974926321, Users1708975325676, Services1708976078071, Appointments1708976549606, Catalogue1708977049545],
+    migrations: [Roles1708974926321, Users1708975325676, Services1708976078071, Catalogue1708977049545, Appointments1708978549606 ],
     synchronize: false,
     logging: false,
 });
