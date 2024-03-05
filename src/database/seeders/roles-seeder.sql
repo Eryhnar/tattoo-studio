efd94e1341b22d7358ec9918ff3817da8e74009e
Eryhnar <pedrofernbel@gmail.com>
2024-03-05 22:51:59 +0100

    feat: added sql seeders.

---
+++ src/database/seeders/roles-seeder.sql
@@ -0,0 +1,6 @@
+USE tattoo_studio;
+
+INSERT INTO roles (name) VALUES ('super_admin');
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('artist');
+INSERT INTO roles (name) VALUES ('customer');
---
+++ src/database/seeders/services-seeder.sql
@@ -0,0 +1,9 @@
+USE tattoo_studio;
+
+insert into services (name, description, photo, created_at, updated_at) values
+('Tattoo from catalogue', 'Description1', now(), now()),
+('Custom Tattoo', 'Description2', now(), now()),
+('Tattoo Restoration', 'Description3', now(), now()),
+('Tattoo Removal', 'Description4', now(), now()),
+('Piercing services', 'Description5', now(), now()),
+('Piercing Supply', 'Description6', now(), now()),
